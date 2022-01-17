f30e4961c15747209871f487c0bafef7d8f8d936
keegal <darkkrage@gmail.com>
2022-01-17 14:27:13 +0000

    seeding complete

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,9 +1,9 @@
-const devData = require('../data/development-data/index.js');
-const seed = require('./seed.js');
-const db = require('../connection.js');
+const devData = require('../data/development-data/');
+const testData = require('../data/test-data')
+const seed = require('./seed');
+const db = require('..');
 
 const runSeed = () => {
   return seed(devData).then(() => db.end());
 };
-
 runSeed();
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,103 @@
+const db = require('../');
+const format = require('pg-format');
+const { at } = require('../data/development-data/categories');
+
 const seed = (data) => {
-  const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+  const { categoryData, commentData, reviewData, userData } = data; let tables = ['categories', 'users', 'reviews', 'comments']
+  let drop = `DROP TABLE IF EXISTS`
+  let create = `CREATE TABLE`
+  let insert = `INSERT INTO`
+  //dropping tables in reverse order:
+  return db.query(`${drop} ${tables[3]};`)
+    .then(() => {
+      return db.query(`${drop} ${tables[2]};`)
+    }).then(() => {
+      return db.query(`${drop} ${tables[1]};`)
+    }).then(() => {
+      return db.query(`${drop} ${tables[0]};`)
+    }).then(() => {
+      console.log('tables dropped')
+      //creating tables:
+      return db.query(`${create} ${tables[0]}(
+        slug VARCHAR PRIMARY KEY NOT NULL,
+        description VARCHAR(255) NOT NULL
+      );
+    `)
+    }).then(() => {
+      return db.query(`${create} ${tables[1]}(
+        username VARCHAR(60) PRIMARY KEY NOT NULL,
+        avatar_url VARCHAR(255),
+        name VARCHAR(255) NOT NULL
+      );
+    `)
+    }).then(() => {
+      return db.query(`${create} ${tables[2]}(
+        review_id SERIAL PRIMARY KEY,
+        title VARCHAR(60) NOT NULL,
+        review_body TEXT NOT NULL,
+        designer VARCHAR(255),
+        review_img_url TEXT,
+        votes INT DEFAULT 0,
+        category VARCHAR REFERENCES categories(slug) NOT NULL,
+        owner VARCHAR(60) REFERENCES users(username) NOT NULL, 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `)
+    }).then(() => {
+      return db.query(`${create} ${tables[3]}(
+        comment_id SERIAL PRIMARY KEY,
+        author VARCHAR(60) REFERENCES users(username) NOT NULL,
+        review_id INT REFERENCES reviews(review_id) NOT NULL,
+        votes INT DEFAULT 0,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        body TEXT        
+      );
+    `)
+    }).then(() => {
+      console.log('tables created')
+      let catArray = categoryData.map(cat => {
+        return [cat.slug, cat.description]
+      })
+      let catItems = format(`${insert} ${tables[0]}
+    (slug, description)
+    VALUES %L
+    RETURNING *;`, catArray)
+      return db.query(catItems)
+    }).then(() => {
+      let userArray = userData.map(user => {
+        return [user.username, user.name, user.avatar_url]
+      })
+      let userItems = format(`${insert} ${tables[1]}
+      (username, name, avatar_url )
+      VALUES %L
+      RETURNING *;`, userArray)
+      return db.query(userItems)
+    }).then(() => {
+      let reviewArray = reviewData.map((rev, index) => {
+        return [rev.title, rev.designer, rev.owner, rev.review_img_url,
+        rev.review_body, rev.category, rev.created_at, rev.votes]
+      })
+      let reviewItems = format(`${insert} ${tables[2]}
+      (title, designer, owner, review_img_url, review_body, category, created_at, votes)
+      VALUES %L
+      RETURNING *;`, reviewArray)
+      return db.query(reviewItems)
+    }).then(() => {
+      let comArray = commentData.map((com, index) => {
+        return [com.body, com.votes, com.author, com.review_id, com.created_at]
+      })
+      let comItems = format(`${insert} ${tables[3]}
+      (body, votes, author, review_id, created_at)
+      VALUES %L
+      RETURNING *;
+      `, comArray)
+      return db.query(comItems)
+    }).then(() => {
+      console.log('data inserted into tables')
+    })
 };
+
+
+
 
 module.exports = seed;
